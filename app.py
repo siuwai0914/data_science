7c1c16ba22c58171e9856847ef1aad5f7019e13b
CHEUNG Siu Wai <siuwai0914@gmail.com>
2025-03-03 18:28:10 +0800

    First commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+import requests
+import json
+from bs4 import BeautifulSoup as soup
+
+app = Flask(__name__)
+
+@app.route("/weather")
+def index():
+    url = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=en"
+    weather = json.loads(requests.get(url).content)
+    date_list = []
+    max_temp_list = []
+    min_temp_list = []
+    
+    for day in weather["weatherForecast"]:
+        date_list.append(day["forecastDate"])
+        max_temp_list.append(day["forecastMaxtemp"]["value"])
+        min_temp_list.append(day["forecastMintemp"]["value"])
+    weather_json = {"date_list": date_list, "max_temp_list": max_temp_list, "min_temp_list": min_temp_list}
+    return render_template("weather_forecast.html", weather_json=weather_json)
+
+
+@app.route("/news")
+def show_news():
+    url = "https://www.scmp.com/rss/40/feed"
+    soup_page = soup(requests.get(url).content, "xml")
+    news_list = soup_page.find_all("item")
+
+    title_list = []
+    link_list = []
+    pubDate_list = []
+    description_list = []
+
+    for news in news_list:
+        title_list.append(news.title.text)
+        link_list.append(news.link.text)
+        pubDate_list.append(news.pubDate.text)
+        description_list.append(news.description.text)
+
+    news_dict = {
+        "title": title_list,
+        "link": link_list,
+        "pubDate": pubDate_list,
+        "description": description_list
+    }
+
+    return render_template("news.html", news_dict=news_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
